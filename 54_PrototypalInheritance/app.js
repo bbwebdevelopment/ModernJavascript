3c1bb4795e23622b4daa97e62cd83d9550cf22ee
Michael Brown <bbwebdeveloper@gmail.com>
2018-09-25 16:18:45 -0700

    added more lesssons

---
+++ 54_PrototypalInheritance/app.js
@@ -0,0 +1,39 @@
+// Person constructor
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+
+}
+
+Person.prototype.greeting = function () {
+  return `Hello there ${this.firstName} ${this.lastName}`;
+}
+
+const person1 = new Person('John', 'Doe')
+console.log(person1.greeting());
+
+//customer constructor
+function Customer (firstName, lastName, phone, membership) {
+  Person.call(this, firstName, lastName);
+
+  this.phone = phone;
+  this.membership = membership;
+}
+
+// inherit the person prototype methods
+Customer.prototype = Object.create(Person.prototype);
+
+//Make customer.prototype return customer()
+Customer.prototype.constructor = Customer;
+
+
+//Create Customer
+const customer1 = new Customer('Tom', 'Smith', '555-555-5555', 'standard');
+
+// console.log(customer1);
+
+//customer greeting
+Customer.prototype.greeting = function() {
+  return `Hello there ${this.firstName} ${this.lastName} welcome to our company`;
+}
+console.log(customer1.greeting());
